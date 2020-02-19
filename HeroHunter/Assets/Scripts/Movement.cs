114525453a6105369673c1e77840246cb70e0766
mmgerik <mmgerik@gmail.com>
2020-02-19 14:54:03 -0600

    Moved pos of grid/props, added movement script

---
+++ HeroHunter/Assets/Scripts/Movement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    bool onGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // sets rb to rigidbody 2d component
+        onGround = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) // checks if player presses left arrow key
+        {
+            transform.Translate(-8 * Time.deltaTime, 0, 0); // moves player to the left
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) // checks if oplayer presses right arrow key
+        {
+            transform.Translate(8 * Time.deltaTime, 0, 0); // moves player to the right
+        }
+
+        if (onGround == true)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow)) // checks if player presss up arrow key
+            {
+
+                rb.AddForce(Vector2.up * 6, ForceMode2D.Impulse); // makes player jump
+                onGround = false;
+
+
+            }
+        }
+
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) //checks collision of object with payer
+    {
+        if (other.tag == "Ground")
+        {
+            onGround = true;
+        }
+
+    }
+
+}
