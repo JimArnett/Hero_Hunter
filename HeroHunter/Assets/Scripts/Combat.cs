86568a51c2ccc7ca72fadf921fc667b70dfada25
JimArnett <jimarnett98@gmail.com>
2020-02-21 16:18:21 -0600

    Fixed combat ish

--- HeroHunter/Assets/Scripts/Combat.cs
+++ HeroHunter/Assets/Scripts/Combat.cs
@@ -12,34 +12,62 @@ public class Combat : MonoBehaviour
     public Slider healthBar;
     public int enemyhealth = 100;
     public Slider enemyhealthBar;
+    public GameObject enemyGO;
+    private IEnumerator coroutine;
+    public bool pressed = false;
+    public GameObject buttonOne;
+    public GameObject buttonTwo;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        if (healthBar.value <= 0){
+            buttonOne.SetActive(false);
+            buttonTwo.SetActive(false);
+            GetComponent<Animator>().SetTrigger("die");
+        }
+        */
     }
 
     public void AttackButton(){
         playerRoll = Random.Range(1,12);
         Debug.Log(playerRoll);
-        if (1 <= playerRoll && playerRoll <= 4){
+        if (1 <= playerRoll && playerRoll <= 4 && pressed == false){
             Debug.Log("Resist");
+            //Instantiate(Resources.Load("Blood"), other.transform.position, other.transform.rotation);
         }
-        if (5 <= playerRoll && playerRoll <= 9){
+        if (5 <= playerRoll && playerRoll <= 9 && pressed == false){
             enemyhealth -= 10;
             enemyhealthBar.value = enemyhealth;
+            //Instantiate(Resources.Load("Blood"), other.transform.position, other.transform.rotation);
         }
-        if (10 <= playerRoll && playerRoll <= 12){
+        if (10 <= playerRoll && playerRoll <= 12 && pressed == false){
             enemyhealth -= 20;
             enemyhealthBar.value = enemyhealth;
+            //Instantiate(Resources.Load("Blood"), other.transform.position, other.transform.rotation);
         }
-        EnemyAttack();
+        pressed = true;
+        /*
+        if (enemyhealthBar.value <= 0){
+            buttonOne.SetActive(false);
+            buttonTwo.SetActive(false);
+            enemyGO.GetComponent<Animator>().SetTrigger("die");
+        }
+        else{
+            coroutine = EnemyAttack(2.0f);
+            StartCoroutine(coroutine);
+        }
+        */
+        coroutine = EnemyAttack(2.0f);
+        StartCoroutine(coroutine);
     }
 
     public void FleeButton(){
@@ -51,18 +79,20 @@ public class Combat : MonoBehaviour
         }
     }
 
-    public void EnemyAttack(){
+    private IEnumerator EnemyAttack(float waitTime){
+        yield return new WaitForSeconds(waitTime);
         playerRoll = Random.Range(1,12);
-        if (1 <= playerRoll && playerRoll <= 6){
+        if (1 <= playerRoll && playerRoll <= 6 && pressed == true){
             health -= 6;
             healthBar.value = health;
         }
-        if (7 <= playerRoll && playerRoll <= 10){
+        if (7 <= playerRoll && playerRoll <= 10 && pressed == true){
             health -= 3;
             healthBar.value = health;
         }
-        if (11 <= playerRoll && playerRoll <= 12){
+        if (11 <= playerRoll && playerRoll <= 12 && pressed == true){
             Debug.Log("Full resist");
         }
+        pressed = false;
     }
 }
