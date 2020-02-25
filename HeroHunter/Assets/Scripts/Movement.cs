1f4086c3d06732e0448cf488b98fbadd4fb2ccae
drhernandez1002 <drhernandez1002@gmail.com>
2020-02-25 13:26:06 -0600

    idk

--- HeroHunter/Assets/Scripts/Movement.cs
+++ HeroHunter/Assets/Scripts/Movement.cs
@@ -8,11 +8,14 @@ public class Movement : MonoBehaviour
     Rigidbody2D rb;
 
     bool onGround;
+    // player walking animation
+    Animator anim;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>(); // sets rb to rigidbody 2d component
         onGround = true;
     }
@@ -23,20 +26,26 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow)) // checks if player presses left arrow key
         {
             transform.Translate(-8 * Time.deltaTime, 0, 0); // moves player to the left
+            anim.SetBool("isWalking", true);
         }
         if (Input.GetKey(KeyCode.RightArrow)) // checks if oplayer presses right arrow key
         {
             transform.Translate(8 * Time.deltaTime, 0, 0); // moves player to the right
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("idle", true);
         }
 
         if (onGround == true)
         {
             if (Input.GetKeyDown(KeyCode.UpArrow)) // checks if player presss up arrow key
             {
-
+             
                 rb.AddForce(Vector2.up * 5, ForceMode2D.Impulse); // makes player jump
                 onGround = false;
-
 
             }
         }
